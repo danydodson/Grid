3f8f424430d044b8574c2d3edcc74c78c81bedbb
danydodson <danydodson@gmail.com>
2023-06-22 01:27:12 -0500

    Initial Commit

---
+++ Grid/GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridApp.swift
+//  Grid
+//
+//  Created by Dany Dodson on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
